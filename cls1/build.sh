06a9bf648c151dd4baa71b5cad3f4b5e66184ba0
sangwf <sangwf@gmail.com>
2013-12-16 13:27:37 +0800

    add checkss

--- cls1/build.sh
+++ cls1/build.sh
@@ -2,6 +2,11 @@ nasm -f bin checkcs.s -o checkcs.bin
 dd conv=sync if=checkcs.bin of=boot.img bs=1440k count=1
 rm -f checkcs.bin
 
+nasm -f bin checkss.s -o checkss.bin
+dd conv=sync if=checkss.bin of=boot.img bs=1440k count=1
+rm -f checkss.bin
+
+
 nasm -f bin printhello.s -o printhello.bin
 dd conv=sync if=printhello.bin of=helloboot.img bs=1440k count=1
 rm -f printhello.bin
---
+++ cls1/checkss.s
@@ -0,0 +1,31 @@
+mov ax, ss ;将ss寄存器的值保存到al
+mov al, ah ;先获取ss的高8位
+add al, 48 ;48是字符0的asc码，这样便于显示
+mov ah, 0x0E ;ah用于bios字符显示中断的类型选择
+             ;0x0e表示显示一个字符
+int 0x10
+
+mov ax, ss ;获取ss的值，主要是低8位
+add al, 48
+mov ah, 0x0E
+int 0x10
+
+mov ax, sp ;将sp寄存器的值保存到al
+mov al, ah ;先获取sp的高8位
+add al, 48 ;48是字符0的asc码，这样便于显示
+mov ah, 0x0E ;ah用于bios字符显示中断的类型选择
+             ;0x0e表示显示一个字符
+int 0x10
+
+mov ax, sp ;获取sp的值，主要是低8位
+add al, 48
+mov ah, 0x0E
+int 0x10
+
+
+hlt ; 让CPU停止运行
+
+times 510 - ($ - $$) db 0 ;填充一堆的0
+                        ;$表示当前位置，$$表示文件头部
+db 0x55
+db 0xAA ;上面两行用于设置引导扇区的标识
